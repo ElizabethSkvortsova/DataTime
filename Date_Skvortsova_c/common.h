317e5da308b3abe51a169428810bb29aff55765e
ElizabethSkvortsova <62102623+ElizabethSkvortsova@users.noreply.github.com>
2020-05-25 20:42:39 +0300

    Add files via upload

---
+++ Date_Skvortsova_c/common.h
@@ -0,0 +1,19 @@
+/* File  Date.h
+Realization of Date structure(Task 26)
+Done by Skvortsova Yelyzaveta
+Date 18.05.2020 */
+
+#include <stdio.h>
+#include <time.h>
+#include <float.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+typedef float DType; /* Double real type for application */
+
+typedef int IType;    /* Integer type for application */
+
+typedef unsigned int NType; /* Unsigned type for application */
+
+typedef struct tm TIME;
